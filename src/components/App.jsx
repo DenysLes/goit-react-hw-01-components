2207d89b3e9395a519379afcf3ec30ccb9f02aec
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-26 17:27:03 +0300

    Update cod add Profile and Statistics sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import user from 'components/Profile/user.json';
+import Profile from '../components/Profile/Profile';
+
+import data from '../components/Statistics/data.json';
+import Statistics from '../components/Statistics/Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </>
   );
 };
+
+//=======================================
+// <div
+//   style={{
+//     height: '100vh',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     fontSize: 40,
+//     color: '#010101',
+//   }}
+// >
+//   React homework template
+// </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { StrictMode } from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// import ReactDOM from 'react-dom/client';
+// import App from '../src/components/App';
+// import { StrictMode } from 'react';
