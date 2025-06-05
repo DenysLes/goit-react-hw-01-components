0ec27b6904a21282433263b5c9ea9d3bd589d946
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-06-05 19:42:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
@@ -10,13 +9,3 @@ root.render(
     <App />
   </StrictMode>
 );
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// import ReactDOM from 'react-dom/client';
-// import App from '../src/components/App';
-// import { StrictMode } from 'react';
